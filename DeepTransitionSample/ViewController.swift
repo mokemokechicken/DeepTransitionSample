ce3f2b21dfb058ffa6e92278ae7af4a02ac8e7e0
Ken Morishita <k_morishita@yumemi.co.jp>
2014-12-13 17:39:08 +0900

    Initial Commit

---
+++ DeepTransitionSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeepTransitionSample
+//
+//  Created by 森下 健 on 2014/12/13.
+//  Copyright (c) 2014年 Yumemi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DeepTransitionSampleTests/DeepTransitionSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  DeepTransitionSampleTests.swift
+//  DeepTransitionSampleTests
+//
+//  Created by 森下 健 on 2014/12/13.
+//  Copyright (c) 2014年 Yumemi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DeepTransitionSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
