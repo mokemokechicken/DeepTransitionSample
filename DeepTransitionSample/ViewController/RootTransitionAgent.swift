26a650ac358e2d1dafcdec4088c69833d804eedd
Ken Morishita <k_morishita@yumemi.co.jp>
2014-12-19 00:41:31 +0900

    today

--- DeepTransitionSample/ViewController/RootTransitionAgent.swift
+++ DeepTransitionSample/ViewController/RootTransitionAgent.swift
@@ -20,7 +20,11 @@ public class RootTransitionAgent : TransitionAgent {
     func start(destinaton: String) {
         transitionCenter.request(destinaton)
     }
-    
+
+    public func decideViewController(pathComponent: TransitionPathComponent) -> UIViewController? {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier(pathComponent.identifier) as? TransitionViewController
+    }
+
     override public func removeViewController(pathComponent: TransitionPathComponent) {
         transitionCenter.reportFinishedRemoveViewControllerFrom(transitionPath)
     }
@@ -29,19 +33,19 @@ public class RootTransitionAgent : TransitionAgent {
         let window = UIApplication.sharedApplication().delegate?.window
         let path = transitionPath.appendPath(component: pathComponent)
  
-        switch pathComponent.identifier {
-        case "top":
-            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("top") as TransitionViewController
-            let nav = UINavigationController(rootViewController: vc)
-            window??.rootViewController = nav
-            window??.makeKeyAndVisible()
-
-            vc.setupAgent(path)
-            transitionCenter.reportViewDidAppear(path)
-            return true
-
-        default:
-            break
+        if let vc = decideViewController(pathComponent) {
+            if let transitionVC = vc as? TransitionViewControllerProtocol {
+                if pathComponent.ownRootContainer == .Navigation {
+                    window??.rootViewController = UINavigationController(rootViewController: vc)
+                } else {
+                    window??.rootViewController = vc
+                }
+                window??.makeKeyAndVisible()
+                
+                transitionVC.setupAgent(path)
+                transitionCenter.reportViewDidAppear(path)
+                return true
+            }
         }
         return false
     }
